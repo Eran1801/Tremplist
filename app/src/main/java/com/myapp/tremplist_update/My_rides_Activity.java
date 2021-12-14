255e90b7ea2bce20122716debcb58504176300f6
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2021-12-14 18:56:58 +0200

    Add my rides on driver page

---
+++ app/src/main/java/com/myapp/tremplist_update/My_rides_Activity.java
@@ -0,0 +1,108 @@
+package com.myapp.tremplist_update;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+// In this class we implement the search result that will show when a passenger want to find a ride
+
+public class My_rides_Activity extends AppCompatActivity {
+    ListView listView;
+
+    FirebaseAuth mAuth;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_results);
+
+        listView = findViewById(R.id.list_view);
+
+        ArrayList<String> ridesList = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(My_rides_Activity.this, R.layout.list_item, ridesList);
+        listView.setAdapter(adapter);
+        
+
+        mAuth = FirebaseAuth.getInstance();
+        DatabaseReference referenceRide = FirebaseDatabase.getInstance().getReference().child("rides");
+
+        referenceRide.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                // if something has left from the last use
+                ridesList.clear();
+                //go over all the rides in the firebase
+                for (DataSnapshot snapshot : datasnapshot.getChildren()) {
+                    Ride ride = snapshot.getValue(Ride.class);
+
+                    assert ride != null; // make sure Ride not null
+                    //check if the current ride is fit to the search details
+                    if (ride.getDriver().id.equals(mAuth.getCurrentUser().getUid())) {
+                        String txt_to_add;
+                        String dest_src = ride.getSrc_city();
+                        if (!ride.getSrc_details().isEmpty())
+                            dest_src += "(" + ride.getSrc_details() + ")";
+                        dest_src += "-->" + ride.getDst_city();
+                        if (!ride.getDst_details().isEmpty())
+                            dest_src += "(" + ride.getDst_details() + ")";
+
+                        String available_sits = "\n" + "מקומות פנויים: " + ride.getFree_sits() + " מתוך " + ride.getSits();
+
+
+                        String hour = "";
+                        if (ride.getHour().getHour() == 0)
+                            hour += "00:";
+                        else
+                            hour += ride.getHour().getHour() + ":";
+                        if (ride.getHour().getMinute() == 0)
+                            hour += "00";
+                        else hour += ride.getHour().getMinute();
+
+                        String date_hour = "\n" + hour + " ," + ride.getDate().getDay() + "/" + ride.getDate().getMonth() + "/" + ride.getDate().getYear();
+
+
+                        String car_details = "";
+                        if (!ride.getCar_color().isEmpty() && !ride.getCar_type().isEmpty())
+                            car_details = "\nפרטי הרכב: " + ride.getCar_type() + " ," + ride.getCar_color();
+                        else if (!ride.getCar_type().isEmpty())
+                            car_details = "\nסוג הרכב: " + ride.getCar_type();
+                        else if (!ride.getCar_color().isEmpty())
+                            car_details = "\nצבע הרכב: " + ride.getCar_color();
+
+
+                        txt_to_add = dest_src + date_hour + available_sits + car_details;
+                        //add the current ride (as a string) to the list
+                        ridesList.add(txt_to_add);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+}
