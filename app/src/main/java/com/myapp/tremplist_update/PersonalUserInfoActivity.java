d4b2320e8de9ddaf1b89435aace308a4103c0c98
Eran1801 <eranlevy9@gmail.com>
2021-12-20 07:54:28 +0200

    deleting unused code

--- app/src/main/java/com/myapp/tremplist_update/PersonalUserInfoActivity.java
+++ app/src/main/java/com/myapp/tremplist_update/PersonalUserInfoActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,15 +20,11 @@ import java.util.Objects;
 
 public class PersonalUserInfoActivity extends AppCompatActivity {
 
-    private EditText first_name,last_name,telephone,_email;
+    private EditText first_name, last_name, telephone, _email;
     Button update;
 
-    // Global var to hold user data inside this activity
-    String USER_FIRST_NAME,USER_LAST_NAME,TELEPHONE,EMAIL;
-
-//    DatabaseReference reference;
-    FirebaseAuth mAuth= FirebaseAuth.getInstance();
-    String UID= Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    String UID = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
 
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://tremplistupdtaefirebase-default-rtdb.firebaseio.com/");
     DatabaseReference myRef = database.getReference("users/" + UID);
@@ -46,8 +41,6 @@ public class PersonalUserInfoActivity extends AppCompatActivity {
         telephone = findViewById(R.id.personal_telephone);
         _email = findViewById(R.id.personal_email);
 
-//        reference = FirebaseDatabase.getInstance().getReference("users");
-
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -62,105 +55,18 @@ public class PersonalUserInfoActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(PersonalUserInfoActivity.this,"error reading from database",Toast.LENGTH_LONG).show();
+                Toast.makeText(PersonalUserInfoActivity.this, "error reading from database", Toast.LENGTH_LONG).show();
             }
         });
 
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveUser(v);
-            }
-        });
+        update.setOnClickListener(v -> saveUser(v));
 
-
-
-
-
-//        showAllUserData();
     }
 
 
     public void saveUser(View view) {
-       User user = new User(first_name.getText().toString(),last_name.getText().toString(), telephone.getText().toString(),_email.getText().toString());
+        User user = new User(first_name.getText().toString(), last_name.getText().toString(), telephone.getText().toString(), _email.getText().toString());
 
-       myRef.setValue(user);
+        myRef.setValue(user);
     }
-
-//    private void showAllUserData(){
-//
-//        Intent intent = getIntent();
-//        USER_FIRST_NAME = intent.getStringExtra("first_name");
-//        USER_LAST_NAME = intent.getStringExtra("last_name");
-//        TELEPHONE = intent.getStringExtra("phone");
-//        EMAIL = intent.getStringExtra("email");
-//
-//        first_name.setText(USER_FIRST_NAME);
-//        last_name.setText(USER_LAST_NAME);
-//        telephone.setText(TELEPHONE);
-//        email.setText(EMAIL);
-//
-//    }
-//
-//    public void update(View view){
-//
-//        if(isNameChanged() || isLastNameChanged() || isTeleChanged() || isEmailChanged() ){
-//            Toast.makeText(this,"Data has been update",Toast.LENGTH_LONG).show();
-//        }
-//        else {
-//            Toast.makeText(this,"Data has same and cannot be update",Toast.LENGTH_LONG).show();
-//        }
-//
-//
-//    }
-//
-//    private boolean isEmailChanged() {
-//
-//        if (!EMAIL.equals(email.getText().toString())){
-//            reference.child(UID).child("email").setValue(email.getText().toString());
-//            EMAIL = email.getText().toString();
-//            return true;
-//        }
-//        else {
-//            return false;
-//        }
-//    }
-//
-//    private boolean isTeleChanged() {
-//
-//        if (!TELEPHONE.equals(telephone.getText().toString())){
-//            reference.child(UID).child("phone").setValue(telephone.getText().toString());
-//            TELEPHONE = telephone.getText().toString();
-//            return true;
-//        }
-//        else {
-//            return false;
-//        }
-//    }
-//
-//    private boolean isLastNameChanged() {
-//
-//        if (!USER_LAST_NAME.equals(last_name.getText().toString())){
-//            reference.child(UID).child("last_name").setValue(last_name.getText().toString());
-//            USER_LAST_NAME = last_name.getText().toString();
-//            return true;
-//        }
-//        else {
-//            return false;
-//        }
-//
-//    }
-//
-//    private boolean isNameChanged() {
-//
-//        if (!USER_FIRST_NAME.equals(first_name.getText().toString())){
-//            reference.child(UID).child("first_name").setValue(first_name.getText().toString());
-//            USER_FIRST_NAME = first_name.getText().toString();
-//            return true;
-//        }
-//        else {
-//            return false;
-//        }
-//
-//    }
 }
