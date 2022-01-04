f98bdad49b93f120d6d64fcc178d6667fc40d086
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2022-01-04 19:04:58 +0200

    add dialog to rate driver

--- app/src/main/java/com/myapp/tremplist_update/UI/MyListAdapter_forTrempist.java
+++ app/src/main/java/com/myapp/tremplist_update/UI/MyListAdapter_forTrempist.java
@@ -1,12 +1,16 @@
 package com.myapp.tremplist_update.UI;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,13 +34,15 @@ public class MyListAdapter_forTrempist extends ArrayAdapter<String> {
     Context ApplicationContext;
     Activity activity;
     FirebaseAuth mAuth;
+    private TextView tv;
+    static Dialog d;
 
     public MyListAdapter_forTrempist(@NonNull Context context, int resource, @NonNull List<String> objects, List<Ride> rides, Context ApplicationContext, Activity activity) {
         super(context, resource, objects);
-        layout=resource;
-        this.rides=rides;
-        this.ApplicationContext=ApplicationContext;
-        this.activity=activity;
+        layout = resource;
+        this.rides = rides;
+        this.ApplicationContext = ApplicationContext;
+        this.activity = activity;
     }
 
 
@@ -46,21 +52,21 @@ public class MyListAdapter_forTrempist extends ArrayAdapter<String> {
         mAuth = FirebaseAuth.getInstance();
         String UID = mAuth.getCurrentUser().getUid();
 
-        ViewHolder_trempist mainViewHolder=null;
-        if(convertView==null){
-            LayoutInflater inflater=LayoutInflater.from(getContext());
-            convertView=inflater.inflate(layout, null, false);
-            ViewHolder_trempist viewHolder=new ViewHolder_trempist();
-            viewHolder.user_img=(ImageView) convertView.findViewById(R.id.user_im_trempist);
-            viewHolder.ride_details=(TextView) convertView.findViewById(R.id.ride_details_trempist);
-            viewHolder.cancel_ride=(androidx.appcompat.widget.AppCompatButton) convertView.findViewById(R.id.cancel_ride_trempist);
+        ViewHolder_trempist mainViewHolder = null;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(layout, null, false);
+            ViewHolder_trempist viewHolder = new ViewHolder_trempist();
+            viewHolder.user_img = (ImageView) convertView.findViewById(R.id.user_im_trempist);
+            viewHolder.ride_details = (TextView) convertView.findViewById(R.id.ride_details_trempist);
+            viewHolder.cancel_ride = (androidx.appcompat.widget.AppCompatButton) convertView.findViewById(R.id.cancel_ride_trempist);
             viewHolder.cancel_ride.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     //TODO NOT SHOULD DELETE FROM TREMPISTS?
-                    Ride curr_ride= new Ride(rides.get(position));
+                    Ride curr_ride = new Ride(rides.get(position));
                     int free_sits = curr_ride.getFree_sits();
-                    curr_ride.setFree_sits(free_sits+1);
+                    curr_ride.setFree_sits(free_sits + 1);
                     fb = new FireBaseDBActivity();
                     fb.setContext(getContext());
                     fb.setApplicationContext(ApplicationContext);
@@ -68,51 +74,67 @@ public class MyListAdapter_forTrempist extends ArrayAdapter<String> {
                     fb.updateRideOnDB_Cancel(curr_ride);
                 }
             });
-            Spinner dropdown = (Spinner) convertView.findViewById(R.id.spinner1);
-            String[] items = new String[]{"בחר דירוג","1", "2", "3","4","5"};
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
-            dropdown.setAdapter(adapter);
-            dropdown.setPrompt("דירוג");
-            dropdown.setSelection(0);
-            viewHolder.rate_ride=(androidx.appcompat.widget.AppCompatButton) convertView.findViewById(R.id.rate_ride);
-            Ride curr_ride = new Ride(rides.get(position));
-            if(curr_ride.getUser_rate().containsKey(UID)){
-                viewHolder.rate_ride.setVisibility(View.GONE);
-                dropdown.setVisibility(View.GONE);
-            }
-            viewHolder.rate_ride.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(dropdown.getSelectedItemPosition() > 0) {
-                        Ride curr_ride = new Ride(rides.get(position));
-                        fb = new FireBaseDBActivity();
-                        fb.setContext(getContext());
-                        fb.setApplicationContext(ApplicationContext);
-                        fb.setActivity(activity);
-                        fb.updateRate(curr_ride,Integer.parseInt(dropdown.getSelectedItem().toString()),UID);
-                        dropdown.setVisibility(View.GONE);
-                        viewHolder.rate_ride.setVisibility(View.GONE);
-                    }
-                    //TODO ADD MESSAGE THAT TELL TO CHOOSE RATE
-                }
-            });
 
+            // If user already rate this ride, don't show this button
+            Ride curr_ride = new Ride(rides.get(position));
+
+
+            viewHolder.rate_ride = (androidx.appcompat.widget.AppCompatButton) convertView.findViewById(R.id.rate_ride);
+            if(curr_ride.getUser_rate().containsKey(UID))
+                viewHolder.rate_ride.setVisibility(View.GONE);
+            viewHolder.rate_ride.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            final Dialog d = new Dialog(getContext());
+                            d.setContentView(R.layout.rate_dialog);
+                            d.setTitle("דרג נהג");
+                            Button cancelBtn = (Button) d.findViewById(R.id.cancel_rateBtn);
+                            Button sendBtn = (Button) d.findViewById(R.id.send_rateBtn);
+                            final NumberPicker np = (NumberPicker) d.findViewById(R.id.ratePicker);
+                            np.setMaxValue(5); // max value 5
+                            np.setMinValue(1);   // min value 1
+                            np.setWrapSelectorWheel(false);
+
+                            // Send Btn
+                            sendBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    System.out.println(String.valueOf(np.getValue()));
+                                    Ride curr_ride = new Ride(rides.get(position));
+                                    fb = new FireBaseDBActivity();
+                                    fb.setContext(getContext());
+                                    fb.setApplicationContext(ApplicationContext);
+                                    fb.setActivity(activity);
+                                    fb.updateRate(curr_ride, np.getValue(), UID);
+                                    d.dismiss();
+                                    viewHolder.rate_ride.setVisibility(View.GONE);
+                                }
+                            });
+                            //Cancel Btn
+                            cancelBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    d.dismiss(); // dismiss the dialog
+                                }
+                            });
+                            d.show();
+                        }
+                    }
+            );
             convertView.setTag(viewHolder);
         }
 
-        mainViewHolder=(ViewHolder_trempist) convertView.getTag();
+        mainViewHolder = (ViewHolder_trempist) convertView.getTag();
         mainViewHolder.ride_details.setText(getItem(position));
-
-
         return convertView;
     }
 
-}
+    class ViewHolder_trempist {
+        ImageView user_img;
+        TextView ride_details;
+        androidx.appcompat.widget.AppCompatButton cancel_ride;
+        androidx.appcompat.widget.AppCompatButton rate_ride;
 
-class ViewHolder_trempist{
-    ImageView user_img;
-    TextView ride_details;
-    androidx.appcompat.widget.AppCompatButton cancel_ride;
-    androidx.appcompat.widget.AppCompatButton rate_ride;
-
+    }
 }
